47bcf9da8435f027ffcf7a4166a606c2d267fefe
jenifferakinyi <otigojeniffer19@gmail.com>
2024-03-12 13:49:25 +0300

    creating server connection to mysql

--- api/helpers/init_mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require ('mongoose')
-
-mongoose.connect(process.env.MONGODB_URI,{dbName:process.env.DB_NAME})
-//promise
-.then(()=>{
-    console.log('connected to MongoDB');
-})
-.catch((err)=>console.log(err.message));
