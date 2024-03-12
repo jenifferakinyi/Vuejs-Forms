0f33ab39fc68dc8b3272e79c433a90ed1a86674f
jenifferakinyi <otigojeniffer19@gmail.com>
2024-03-12 10:22:50 +0300

    making the navbar

--- users/src/views/HomeView.vue
+++ users/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <h1 class="text-center text-2xl"> WELCOME HOME</h1>
   </main>
 </template>
