6c0ee986925244d550a697f35f6f2c4e6e8d1d23
jenifferakinyi <otigojeniffer19@gmail.com>
2024-03-13 09:50:00 +0300

    changing server connection from msql to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+
+// const cors = require('cors');
 require('dotenv').config();
+require('./helpers/init_mongodb');
+
 const app = express();
 
-// // CORS configuration
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-// };
-// app.use(cors(corsOptions));
+// // Enable CORS for all routes
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true, // enable set cookie
+// }));
 
-// Body parsing middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
 
 
 // Handling error 404 (if no matching route found)
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).send({
-    error: {
-      status: err.status || 500,
-      message: err.message || 'Internal Server Error',
-    },
-  });
+    res.status(err.status || 500)
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+app.listen(process.env.PORT || 4000, function () {
+    console.log("now listening for requests on: http://localhost:4000");
 });
