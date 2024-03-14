dde27438fc5785e2d70eee5eaac8ef94a90ab42a
jenifferakinyi <otigojeniffer19@gmail.com>
2024-03-14 23:47:54 +0300

    making forgot password form

---
+++ users/src/views/ForgotpasswordView.vue
@@ -0,0 +1,13 @@
+<!-- ForgotPassword.vue -->
+<template>
+    <div>
+      <h2>Forgot Password</h2>
+      <form @submit.prevent="sendResetEmail">
+        <input type="email" v-model="email" placeholder="Enter your email" required>
+        <button type="submit">Send Reset Email</button>
+      </form>
+    </div>
+  </template>
+  
+
+  
