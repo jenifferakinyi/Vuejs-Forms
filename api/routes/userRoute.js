b3a346e45b3e4ebd95abef2a58662077c1087374
jenifferakinyi <otigojeniffer19@gmail.com>
2024-03-15 00:12:26 +0300

    adding route for reset pass

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -10,6 +10,7 @@ const userController = require ('../controller/userController');
 routes.post('/user', userController.AddUser);
 
 routes.post('/login', userController.Login);
+router.post('/reset-password/:token', userController.resetPassword);
 
 routes.post('/refresh', userController.refreshToken);
 
